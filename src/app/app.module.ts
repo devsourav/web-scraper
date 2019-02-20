40a4f5428da03af96d16459964539a038ac3e882
sourav das <sourav@pixelette.in>
2019-02-21 04:29:35 +0530

    Added ngx browser storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxWebstorageModule } from 'ngx-webstorage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { environment } from '../environments/environment';
 import { RestService } from './services/rest.service';
 import { FileService } from './services/file.service';
 import { SnackMessageService } from './services/snack-message.service';
@@ -23,6 +24,7 @@ import { SnackMessageService } from './services/snack-message.service';
     BrowserAnimationsModule,
     LayoutModule,
     MatSnackBarModule,
+    NgxWebstorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
